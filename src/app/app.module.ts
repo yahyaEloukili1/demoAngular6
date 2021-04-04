d0cab09a71a2d0500842e390235f0770ea99e3a4
yahya <yahya.ensas@hotmail.com>
2021-04-04 16:47:46 +0100

    part5 angular rective forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
 import { ListEmployeesComponent } from './employee/list-employees/list-employees.component';
-
+import { ReactiveFormsModule } from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +14,7 @@ import { ListEmployeesComponent } from './employee/list-employees/list-employees
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup,FormControl } from "@angular/forms";
+import { TestService } from "../../services/test.service";
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
+  employeeForm : FormGroup;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.employeeForm = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl()
+    })
 
+  }
+  onSubmit(): void{
+    console.log(this.employeeForm.controls.fullName.value)
+    console.log(this.employeeForm.get('fullName').value)
+  }
 }
