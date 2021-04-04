eebf90d522b50135d4a7ce23170fb5c82a872211
yahya <yahya.ensas@hotmail.com>
2021-04-04 15:11:33 +0100

    Angular 6 routing tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {  RouterModule ,Route} from '@angular/router';
+import { ListEmployeesComponent } from "../app/employee/list-employees/list-employees.component";
+import { CreateEmployeeComponent } from "../app/employee/create-employee/create-employee.component"
+const routes: Route[] = [
+  {path: 'list',component: ListEmployeesComponent},
+  {path: 'create',component: CreateEmployeeComponent},
+  {path: '', redirectTo: '/list',pathMatch:'full'},
+];
 
 @NgModule({
+  //Tell the angular router about these three routes
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
