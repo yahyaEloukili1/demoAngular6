cf4ae7bb0c8f1393e52fd4826f3e5629aa0ec0e7
yahya <yahya.ensas@hotmail.com>
2021-04-05 12:45:14 +0100

    part13 moving validation to ts file

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -46,16 +46,18 @@ export class CreateEmployeeComponent implements OnInit {
         proficiency : ['',Validators.required]
       })
     })
-
+this.employeeForm.valueChanges.subscribe(data=>{
+  this.logValidationErrors(this.employeeForm)
+})
   }
-  logValidationErrors(group: FormGroup): void{
+  logValidationErrors(group: FormGroup = this.employeeForm): void{
       Object.keys(group.controls).forEach((key : string)=>{
        const abstractControl =  group.get(key)
        if(abstractControl instanceof FormGroup){
          this.logValidationErrors(abstractControl)
        }else{
         this.formErrors[key] = ''
-        if(abstractControl && !abstractControl.valid){
+        if(abstractControl && !abstractControl.valid &&( abstractControl.touched || abstractControl.dirty)){
           const messages = this.validationMessages[key];
           for(const errorKey in abstractControl.errors){
             if(errorKey){
@@ -68,12 +70,12 @@ export class CreateEmployeeComponent implements OnInit {
 
   }
   onSubmit(): void{
-    console.log(this.employeeForm.controls.fullName.value)
-    console.log(this.employeeForm.get('fullName').errors)
-    console.log(this.employeeForm.value)
+    // console.log(this.employeeForm.controls.fullName.value)
+    // console.log(this.employeeForm.get('fullName').errors)
+    // console.log(this.employeeForm.value)
   }
   onLoadDataClick(){
-    this.logValidationErrors(this.employeeForm);
-    console.log(this.formErrors)
+    // this.logValidationErrors(this.employeeForm);
+    // console.log(this.formErrors)
   }
 }
